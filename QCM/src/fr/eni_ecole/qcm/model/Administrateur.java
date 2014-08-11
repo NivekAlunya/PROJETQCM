9b38412df886dd7d6972952949f2191797cc7f98
unknown <ml17eni@gmail.com>
2014-08-11 17:23:14 +0200

    création classe Adminisrateur

---
+++ QCM/src/fr/eni_ecole/qcm/model/Administrateur.java
@@ -0,0 +1,85 @@
+package fr.eni_ecole.qcm.model;
+
+import java.io.Serializable;
+
+public class Administrateur implements Serializable
+{
+	
+	// definition des attributs
+	private int idAdministrateur;
+	private String nom;
+	private String prenom;
+	private String motDePasse;
+	private String email;
+	
+	// Constructeurs
+	
+	 public Administrateur()
+	{
+		 this(0,null,null,null,null);
+	}
+	public Administrateur(int idAdministrateur, String nom, String prenom,
+				String motDePasse, String email)
+	{
+		super();
+		this.idAdministrateur = idAdministrateur;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.motDePasse = motDePasse;
+		this.email = email;
+	}
+
+	// méthodes Get
+	
+	public int getIdAdministrateur()
+	{
+		return idAdministrateur;
+	}
+	
+	public String getNom() 
+	{
+		return nom;
+	}
+	
+	public String getPrenom()
+	{
+		return prenom;
+	}
+	
+	public String getMotDePasse()
+	{
+		return motDePasse;
+	}
+			
+	public String getEmail()
+	{
+		return email;
+	}
+	
+	// méthodes Set
+	public void setIdAdministrateur(int idAdministrateur)
+	{
+		this.idAdministrateur = idAdministrateur;
+	}
+		
+	public void setNom(String nom) 
+	{
+		this.nom = nom;
+	}
+	
+	public void setPrenom(String prenom) 
+	{
+		this.prenom = prenom;
+	}
+	
+	public void setMotDePasse(String motDePasse) 
+	{
+		this.motDePasse = motDePasse;
+	}
+	
+	public void setEmail(String email)
+	{
+		this.email = email;
+	}
+
+}
