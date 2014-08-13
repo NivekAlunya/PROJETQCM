68a9723f81ef0e06577442557c976117db94b706
unknown <klaunay.private@gmail.com>
2014-08-13 09:32:22 +0200

    fix modele promotion

--- QCM/src/fr/eni_ecole/qcm/model/Promotion.java
+++ QCM/src/fr/eni_ecole/qcm/model/Promotion.java
@@ -3,31 +3,31 @@ package fr.eni_ecole.qcm.model;
 import java.io.Serializable;
 
 public class Promotion implements Serializable{
-	private String codepromotion;
+	private String codePromotion;
 	private String libelle;
 	
 	
 	@Override
 	public String toString() {
-		return "Promotion [codepromotion=" + codepromotion + ", libelle="
+		return "Promotion [codepromotion=" + codePromotion + ", libelle="
 				+ libelle + "]";
 	}
 
 	public Promotion(String codepromotion, String libelle) {
 		super();
-		this.codepromotion = codepromotion;
+		this.codePromotion = codepromotion;
 		this.libelle = libelle;
 	}
 	
 	public Promotion () {
-		this(null, null);
+		this("----------", "");
 	}
 	
-	public String getCodepromotion() {
-		return codepromotion;
+	public String getCodePromotion() {
+		return codePromotion;
 	}
-	public void setCodepromotion(String codepromotion) {
-		this.codepromotion = codepromotion;
+	public void setCodePromotion(String codepromotion) {
+		this.codePromotion = codepromotion;
 	}
 	public String getLibelle() {
 		return libelle;
