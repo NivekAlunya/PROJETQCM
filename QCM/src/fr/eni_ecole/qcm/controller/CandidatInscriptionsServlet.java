760b2ad07c7aa8ebac8a09b483f3c360fa7d5c2e
unknown <ml17eni@gmail.com>
2014-08-20 16:55:09 +0200

    modif servlet inscriptioncandidat

--- QCM/src/fr/eni_ecole/qcm/controller/CandidatInscriptionsServlet.java
+++ QCM/src/fr/eni_ecole/qcm/controller/CandidatInscriptionsServlet.java
@@ -18,9 +18,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import fr.eni_ecole.qcm.model.Test;
+import fr.eni_ecole.qcm.model.*;
 
-import fr.eni_ecole.qcm.store.TestStore;
+import fr.eni_ecole.qcm.store.*;
 
 /**
  * Servlet implementation class ListerTests
@@ -74,20 +74,23 @@ public class CandidatInscriptionsServlet extends HttpServlet {
 	protected void processRequest(HttpServletRequest request,
 		HttpServletResponse response) throws Exception {
 		System.out.println("processRequest " + response + " " + request);
-	
-		ArrayList<Test> listeTest = new ArrayList<Test>();
+		Candidat c = (Candidat)request.getSession().getAttribute("candidat");
+		
+		ArrayList<Inscription> listeInscription;
 		// Construire la liste des tests et la placer en session
 		try {
-			listeTest = TestStore.listeTest();
-			System.out.println(listeTest);
-		}catch (SQLException e){
+			listeInscription = InscriptionStore.rechercherInscription(c.getIdCandidat());
+			System.out.println(listeInscription);
+			request.setAttribute("inscriptions", listeInscription);			
+			RequestDispatcher dispatcher= null;
+			dispatcher = request.getRequestDispatcher("/CandidatInscriptions.jsp"); 
+			dispatcher.forward(request, response);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		request.setAttribute("tests", listeTest);			
-		RequestDispatcher dispatcher= null;
-		dispatcher = request.getRequestDispatcher("/CandidatInscriptions.jsp"); 
-		dispatcher.forward(request, response);
+		
 		
 	}
 		
